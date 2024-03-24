f747d2d55e8d364c79d07d4da6bf22b1a97e7ff6
Erick Valdir Machicado Calle <etyvaldirmc@gmail.com>
2024-03-23 20:41:31 -0400

    modified - local product cart

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -6,7 +6,9 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   private items: any[] = []
-  constructor() { }
+  constructor() {
+    this.items = (localStorage.getItem('cart')) ? this.getItems() : [];
+  }
 
   addItem(item: any) :void {
     if (this.items.length < 1) {
@@ -18,22 +20,37 @@ export class CartService {
         this.items = this.items.map(val => (val.Id === item.Id) ? {...val, Amount: val.Amount+1 } : val)
       }
     }
+    localStorage.setItem('cart', JSON.stringify(this.items));
   }
 
   removeItem(item: any) :void {
     if (this.items.length < 1) {
       this.items = this.items.filter(val => (val.Id === item.Id))
+      localStorage.removeItem('cart')
     } else {
       if (this.items.some(val => (val.Id === item.Id && val.Amount > 1))) {
         this.items = this.items.map(val => (val.Id === item.Id) ? {...val, Amount: (val.Amount - 1) } : val)
       } else {
         this.items = this.items.filter(val => !(val.Id === item.Id))
       }
+      localStorage.setItem('cart', JSON.stringify(this.items));
     }
   }
 
-  getItems() : any[] {
-    return this.items;
+  getItems(): any[] {
+    return (localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')!) : [];
+  }
+
+  getTotals(){
+    let cart = {
+      priceTotal: 0,
+      amountTotal: 0,
+    }
+    this.items.map((item) => {
+      cart.priceTotal = cart.priceTotal + item.Price
+      cart.amountTotal = cart.amountTotal + item.Amount
+    })
+    return cart
   }
 
   clearCart() :void {
